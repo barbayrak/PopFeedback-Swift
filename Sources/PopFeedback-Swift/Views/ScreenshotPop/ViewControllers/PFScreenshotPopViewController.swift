ca5c38f469ace631128e586e9477e2e2266a2a31
Kaan Baris BAYRAK <kaan.bayrak@ozu.edu.tr>
2022-10-19 20:58:29 +0200

    PFScreenshot & PFRate Views are implemented

--- Sources/PopFeedback-Swift/Views/ScreenshotPop/ViewControllers/PFScreenshotPopViewController.swift
+++ Sources/PopFeedback-Swift/Views/ScreenshotPop/ViewControllers/PFScreenshotPopViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class PFScreenshotPopViewController: UIViewController {
-
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var bugReportButton: UIButton!
+    @IBOutlet weak var bugReportLabel: UILabel!
+    @IBOutlet weak var feedbackButton: UIButton!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var shareLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
